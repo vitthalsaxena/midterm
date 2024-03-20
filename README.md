ea2a1185408ef9247cc46416ffe64da793408bf2
Vitthal Saxena <visaxen@iu.edu>
2024-03-20 01:39:14 +0000

    Midterm v2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+**Loading the csv data into BigQuery
 bq load --autodetect --source_format=CSV
 midterm-visaxen:pdf_files.file-list
 gs://visaxen-pdf-source/PDFMalware2022.csv
--- index.js
+++ index.js
@@ -3,72 +3,50 @@ const {BigQuery}=require('@google-cloud/bigquery');
 
 //Loading into BigQuery for test on demo file
 const bq=new BigQuery();
+const projId='midterm-visaxen';
 const datasetId='pdf_files';
 const tableId='file-list';
-
+let count_malicious = 0;
+let count_benign = 0; 
 
 exports.classifyFiles = async (file,context) => {
+  const gcsFile = file;
+  const fname = gcsFile.name;
   const storage = new Storage();
-  const testBucket = storage.bucket('visaxen-pdftest');
+  const testBucket = storage.bucket(file.bucket);
   const maliBucket = storage.bucket('visaxen-pdftest-malicious');
   const benBucket = storage.bucket('visaxen-pdftest-benign');
-  let count_malicious = 0;
-  let count_benign = 0; 
 
-  sortFiles();
-  testRun();
-
-  //Helper function to read filenames from BigQuery and sort them into respective class bucket
-  const sortFiles = async () => {
-    // The SQL query to run
-    const query = `SELECT * FROM ${datasetId}.${tableId} LIMIT 2500`;
-    const options = {
-      query: query
-    };
-    // Run the query
-    const [rows] = await bq.query(options);
-
-    rows.forEach(row => {
-      if(row.Class == 'Malicious'){
-        count_malicious = count_malicious+1;
-        copyFile(row.filename, maliBucket);
-      }
-      else if(row.Class == 'Benign'){
-        count_benign = count_benign+1;
-        copyFile(row.filename, benBucket);
-      }
-    })
-
-    console.log(`Malicious Files: ${count_malicious}`);
-    console.log(`Benign Files: ${count_benign}`);
-  }
-
-  // Function to copy file from the test bucket to a destination bucket
-  async function copyFile(filename, destBucket) {
-    await storage.bucket(testBucket).file(filename).copy(storage.bucket(destBucket).file(filename));
+  //Function to copy the path from test bucket to the destination bucket
+  const copyFile = async (destBucket) => {
+    const srcFile = testBucket.file(fname);
+    const destFile = destBucket.file(fname);
+    await srcFile.copy(destFile);
   }
 
   //Helper function to check if the file is present in the database and return its class
-  const scanFiles = async (filename) => {
-    const query = `SELECT filename, Class FROM ${datasetId}.${tableId} WHERE filename = ${filename}`;
+  const scanFiles = async () => {
+    const query = `SELECT filename, Class FROM \`midterm-visaxen.pdf_files.file-list\` WHERE filename = '${fname}'`;
     const options = {
       query: query
     };
 
-    const [rows] = await bigquery.query(options);
+    const [rows] = await bq.query(options);
     rows.forEach(row => {
-      if(row.count > 0)
-        console.log(`${filename}: ${row.Class}`);
-      else
-        console.log("File Not Present");
-    })
-  }
-
-  //Helper function to go through the files in the test bucket
-  async function testRun () {
-    const [files] = await testBucket.getFiles();
-    files.forEach(file => {
-      scanFiles(file.name);
+      if(row.Class == 'Malicious'){
+        count_malicious++;
+        copyFile(maliBucket);
+      }
+      else if(row.Class == 'Benign'){
+        count_benign++;
+        copyFile(benBucket);
+      }
     });
   }
+
+  await scanFiles();
+  console.log(`Malicious Files: ${count_malicious}`);
+  console.log(`Benign Files: ${count_benign}`);
+
+  await testBucket.file(fname).delete();
 }
